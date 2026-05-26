42ddd2cd80a3a995466692f64dd9930358350959
Abdullah Elkuse <abdooo1997003@gmail.com>
2026-05-26 08:13:16 +0300

    test: add domain unit tests

---
+++ AE.Market.Domain.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+﻿namespace AE.Market.Domain.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
